f83b509ac4f0263bc018fd07f79dbbda7e5bc0ae
HardikAgrwl <hardikagarwal213227@example.com>
2021-08-01 17:03:46 +0530

    redux store setup and action creators added

---
+++ client/src/actions/itemActions.js
@@ -0,0 +1,72 @@
+import axios from "axios";
+import { returnErrors } from "./errorActions";
+import {
+  ADD_ITEM,
+  DELETE_ITEM,
+  GET_ITEMS,
+  ITEMS_LOADING,
+  UPDATE_ITEM,
+} from "./types";
+
+export const getItems = () => (dispatch) => {
+  dispatch(setItemsLoading());
+  axios
+    .get("/api/items")
+    .then((res) =>
+      dispatch({
+        type: GET_ITEMS,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
+};
+
+export const addItem = (item) => (dispatch) => {
+  axios
+    .post("/api/items", item)
+    .then((res) =>
+      dispatch({
+        type: ADD_ITEM,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
+};
+
+export const deleteItem = (id) => (dispatch) => {
+  axios
+    .delete(`/api/items/${id}`)
+    .then((res) =>
+      dispatch({
+        type: DELETE_ITEM,
+        payload: id,
+      })
+    )
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
+};
+
+export const updateItem = (id, item) => (dispatch) => {
+  axios
+    .put(`/api/items/${id}`, item)
+    .then((res) =>
+      dispatch({
+        type: UPDATE_ITEM,
+        payload: Promise.all([id, res.data]),
+      })
+    )
+    .catch((err) =>
+      dispatch(returnErrors(err.response.data, err.response.status))
+    );
+};
+
+export const setItemsLoading = () => {
+  return {
+    type: ITEMS_LOADING,
+  };
+};
